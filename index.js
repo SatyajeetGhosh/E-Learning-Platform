6591fbd6176b179b367bf510ccc919c197a55a9a
Satyajeet Ghosh <103898124+SatyajeetGhosh@users.noreply.github.com>
2023-02-13 21:48:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+togglebtn = document.querySelector(".hamburger");
+togglebtn.onclick = function(){
+    navbar = document.querySelector(".nav-links");
+    navbar.classList.toggle("active");
+}
+
+const swiper = new Swiper('.swiper',{
+    loop:true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
