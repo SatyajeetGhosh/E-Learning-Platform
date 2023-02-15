9f91350e75dae2c28aec93e3beb1d4d09dda27ed
SatyajeetGhosh <satyajeetghosh5331@gmail.com>
2023-02-15 12:03:42 +0530

    Added contact.js

--- index.js
+++ index.js
@@ -16,22 +16,3 @@ const swiper = new Swiper('.swiper',{
         prevEl: '.swiper-button-prev',
     },
 });
-
-const inputs = document.querySelectorAll(".input");
-
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
-
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-});
